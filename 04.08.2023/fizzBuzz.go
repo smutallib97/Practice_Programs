0739f6b585d6fe005272a9b97b3217664d94a853
Sayyed Mutallib <smutallib97@gmail.com>
2023-08-07 23:14:12 +0530

    fizzBuzz

--- 04.08.2023/fizzBuzz.go
+++ 04.08.2023/fizzBuzz.go
@@ -1,4 +1,4 @@
-//FizzBuzz: Print numbers from 1 to n, but for multiples of 3,
+//Print numbers from 1 to n, but for multiples of 3,
 //print "Fizz," for multiples of 5, print "Buzz," and for multiples
 //of both 3 and 5, print "FizzBuzz."
 
